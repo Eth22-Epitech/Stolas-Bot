a7c8471084a35803876886c572543644b0c22eaa
Flak Ethan <ethan.flak@epitech.eu>
2024-07-25 11:34:04 +0200

    [+] Add line to stop previously running container in run_docker.sh shell script

--- run_docker.sh
+++ run_docker.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# Step 1: Remove previous container
+# Step 1: Stop the Docker container
+docker stop stolas_bot_container
+
+# Step 2: Remove previous container
 docker rmi -f stolas_bot_image
 
-# Step 2: Remove previous image
+# Step 3: Remove previous image
 docker rm -f stolas_bot_container
 
-# Step 3: Build the Docker image
+# Step 4: Build the Docker image
 docker build -t stolas_bot_image .
 
-# Step 4: Run the Docker container
+# Step 5: Run the Docker container
 docker run -d --name stolas_bot_container -v $(pwd)/logs:/stolas_bot/logs stolas_bot_image
