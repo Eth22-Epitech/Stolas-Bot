2aeac3db3a0fc07233b70484ac9af905ea35f788
Flak Ethan <ethan.flak@epitech.eu>
2024-07-25 12:24:17 +0200

    [~] Modify run_docker.sh shell script to use docker-compose to run the bot

--- run_docker.sh
+++ run_docker.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# Step 1: Stop the Docker container
-docker stop stolas_bot_container
+# Step 1: Stop and remove the previous container and image if exists
+docker-compose down --remove-orphans
 
-# Step 2: Remove previous container
-docker rmi -f stolas_bot_image
+# Step 2: Build the Docker image using docker-compose
+docker-compose build --remove-orphans
 
-# Step 3: Remove previous image
-docker rm -f stolas_bot_container
-
-# Step 4: Build the Docker image
-docker build -t stolas_bot_image .
-
-# Step 5: Run the Docker container
-docker run -d --name stolas_bot_container -v $(pwd)/logs:/stolas_bot/logs stolas_bot_image
+# Step 3: Run the Docker container
+docker-compose up -d
