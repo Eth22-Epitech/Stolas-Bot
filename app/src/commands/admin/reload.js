f9dc795c48d2bd80b831cf80471a9d154b7e8c4a
Flak Ethan <ethan.flak@epitech.eu>
2024-08-11 01:00:36 +0200

    [+] Add reload command for easier development

---
+++ app/src/commands/admin/reload.js
@@ -0,0 +1,44 @@
+const { SlashCommandBuilder } = require('discord.js');
+const logger = require('../../logger.js');
+const { admin_users } = require('../../config.json');
+const moment = require('moment');
+
+module.exports = {
+    category: 'utility',
+    data: new SlashCommandBuilder()
+        .setName('reload')
+        .setDescription('Reloads a command.')
+        .addStringOption(option =>
+            option.setName('command')
+                .setDescription('The command to reload.')
+                .setRequired(true)),
+    async execute(interaction) {
+        const now = moment().format('MM/DD/YYYY HH:mm:ss');
+
+        const commandName = interaction.options.getString('command', true).toLowerCase();
+        const command = interaction.client.commands.get(commandName);
+
+        if (!admin_users.includes(interaction.user.id)) {
+            logger.log('info', `${now} - ${interaction.user.username} (${interaction.user.id}) '/reload ${commandName}' in '${interaction.guild.name} #${interaction.channel.name}' issued => NOT ADMIN`);
+            return interaction.reply({content: `You are not an Admin of Stolas Bot.`, ephemeral: true});
+        }
+
+        if (!command) {
+            logger.log('info', `${now} - ${interaction.user.username} (${interaction.user.id}) '/reload ${commandName}' in '${interaction.guild.name} #${interaction.channel.name}' issued => Invalid command name`);
+            return interaction.reply(`There is no command with name \`${commandName}\`!`);
+        }
+
+        delete require.cache[require.resolve(`../${command.category}/${command.data.name}.js`)];
+
+        try {
+            interaction.client.commands.delete(command.data.name);
+            const newCommand = require(`../${command.category}/${command.data.name}.js`);
+            interaction.client.commands.set(newCommand.data.name, newCommand);
+            logger.log('info', `${now} - ${interaction.user.username} (${interaction.user.id}) '/reload ${commandName}' in '${interaction.guild.name} #${interaction.channel.name}' issued => Reloaded`);
+            return interaction.reply({content: `Command \`${newCommand.data.name}\` was reloaded!`, ephemeral: true});
+        } catch (error) {
+            logger.log('error', `${now} - ${interaction.user.username} (${interaction.user.id}) '/reload ${commandName}' in '${interaction.guild.name} #${interaction.channel.name}' issued => Caught Error`);
+            return interaction.reply({content: `There was an error while reloading a command \`${command.data.name}\`:\n\`${error.message}\``, ephemeral: true});
+        }
+    },
+};
